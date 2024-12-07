b0f2441f189706bf5210ef659cbb0a795b904844
OFFICIALNITIN <official.nitin105@gmail.com>
2024-12-07 23:09:53 +0530

    made changes in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://expense-tracker-server-xyug.onrender.com",
         changeOrigin: true,
       },
     },
